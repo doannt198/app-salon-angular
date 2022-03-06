21aa76251adc7bdbe9b9a571e544a0604b031bef
TienDoan <nguyentiendoanhn09@gmail.com>
2022-03-06 15:43:15 +0700

    121

--- src/app/voucher-management/them-voucher/them-voucher.component.ts
+++ src/app/voucher-management/them-voucher/them-voucher.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import {MenuItem} from 'primeng/api'
 @Component({
   selector: 'app-them-voucher',
   templateUrl: './them-voucher.component.html',
   styleUrls: ['./them-voucher.component.scss']
 })
 export class ThemVoucherComponent implements OnInit {
-
+  items: MenuItem[];
+  activeItem: MenuItem;
   constructor() { }
 
   ngOnInit(): void {
+    this.items = [
+      {label: 'Thông tin'},
+      {label: 'Gán dữ liệu'},
+      
+  ];
+    this.activeItem = this.items[0];
   }
 
 }
